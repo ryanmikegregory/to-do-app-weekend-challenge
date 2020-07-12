e0e8b631d787f775a0673cfa6c151f3b7ea20212
Ryan M Gregory <ryan@ryanmgregory.com>
2020-07-11 21:53:46 -0500

    created router file, brought in neccesary dependencies, and exported into server with future url for ajax requests

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser'); //body-parser for json correctness
 const pg = require('pg'); //postgres
 const app = express();
 
+const todoRouter = require('./routes/todo.router');
+
 const PORT = 5005;
 
 //ROUTES//
@@ -10,6 +12,7 @@ const PORT = 5005;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('server/public'));
+app.use('/api/todo', todoRouter);
 
 app.listen(PORT, () => {
   console.log(`listening on PoRt: ${PORT}`);
