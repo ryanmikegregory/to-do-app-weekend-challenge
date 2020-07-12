37bfb70429016fd21e05b695b2635cba12909e25
Ryan M Gregory <ryan@ryanmgregory.com>
2020-07-11 22:31:10 -0500

    created click handeling of user input payloadObject and created client side Ajax POST route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,12 +5,32 @@ $(document).ready(init);
 //TODO click events
 function init() {
   console.log(`$ReAdY, in init!`);
+  $('#new-task-form').on('click', '#addTask-btn', submitAddTask);
 }
 
-//TODO capture user input
+//TODO capture user input EVENT
+function submitAddTask() {
+  let payloadObject = {
+    task_name: $('#new-task-input').val(),
+    completed: false,
+  };
+}
 
 //TODO send to server and then database
 
+//Ajax POST ROUTE
+function postTodo() {
+  $.ajax({
+    type: 'POST',
+    url: '/api/todo',
+    data: todoData,
+  })
+    .then((dbResponse) => {
+      //get stuff//render call goes here renderList(dbResponse)
+    })
+    .catch((error) => {
+      console.log(`No bueno in POST-client ${error}`);
+    });
+}
+
 //TODO retrieve from database and append task to page with delete and complete btns
-
-
