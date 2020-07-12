ae5e0952d16388891f39b5a5a20cd950fc5e7bf4
Ryan M Gregory <ryan@ryanmgregory.com>
2020-07-11 22:10:18 -0500

    TODOs on client created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,15 @@ console.log('in client');
 
 $(document).ready(init);
 
+//TODO click events
 function init() {
   console.log(`$ReAdY, in init!`);
 }
+
+//TODO capture user input
+
+//TODO send to server and then database
+
+//TODO retrieve from database and append task to page with delete and complete btns
+
+
